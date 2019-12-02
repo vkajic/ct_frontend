3d0416b7189e8644c5695764029ff615ec4ba498
mario <tadic.mario@gmail.com>
2019-12-02 22:24:50 +0100

    tags display fix

--- src/components/ui/TagsDisplay.vue
+++ src/components/ui/TagsDisplay.vue
@@ -2,7 +2,8 @@
   <ul class="list-tags list-unstyled list-inline">
     <li class="list-inline-item" v-for="(tag, index) in tags" :key="index">
       <template v-if="linkable">
-        <a class="tag-item mb-2" href="#" @click.prevent="$emit('click', tag)">{{tag.name}}</a>
+        <a class="tag-item d-block mb-2" href="#"
+           @click.prevent="$emit('click', tag)">{{tag.name}}</a>
       </template>
       <template v-else>
         <div class="tag-item mb-2">{{tag.name}}</div>
