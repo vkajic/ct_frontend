825bbcd649fa3645234c8964ad02cd9a295d20ac
igor-lid <onaj.igor@gmail.com>
2020-03-25 18:55:24 +0100

    implement responsive menu

---
+++ src/components/layout/MobileTopMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-dropdown class="d-xl-none" variant="link" no-caret right toggle-class="p-0" v-if="!user">
+    <template slot="button-content">
+        <span class="d-block">
+              <menu-icon size="1.5x" />
+        </span>
+    </template>
+    <b-dropdown-item to="/tasks">Browse Jobs</b-dropdown-item>
+    <b-dropdown-item to="/freelancers">Browse Freelancers</b-dropdown-item>
+    <b-dropdown-item to="/login">Post Job</b-dropdown-item>
+    <b-dropdown-item to="landingUrl">How It Works</b-dropdown-item>
+    <b-dropdown-divider/>
+    <b-dropdown-item to="/login">Login</b-dropdown-item>
+    <b-dropdown-item to="/sign-up">Sign up</b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<script>
+import { MenuIcon } from 'vue-feather-icons';
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+  name: 'MobileTopMenu',
+  components: {
+    MenuIcon,
+  },
+  computed: {
+    user() {
+      return this.$store.state.user.user;
+    },
+    landingUrl() {
+      return `${process.env.VUE_APP_LANDING_URL}how-cryptotask-works`;
+    },
+  },
+};
+</script>
