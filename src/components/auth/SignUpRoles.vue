b5248607317c83721a143d72d67abad26717ba77
igor-lid <onaj.igor@gmail.com>
2020-03-26 13:33:00 +0100

    fix divider spacing

--- src/components/auth/SignUpRoles.vue
+++ src/components/auth/SignUpRoles.vue
@@ -4,11 +4,12 @@
 
     <div class="roles-chooser d-flex align-items-stretch h-100">
       <a href="#"
-         class="role h-100 w-50 d-flex align-items-center border-right py-5 justify-content-center"
+         class="role h-100 w-50 d-flex align-items-center py-5 justify-content-center"
          @click.prevent="$emit('select', 'freelancer')">
         <coffee-icon size="3x" class="mr-3"/>
         <span>I'm a<br/>Freelancer</span>
       </a>
+      <hr class="m-0 mx-3 w-1 h-auto border-right" />
       <a href="#"
          class="role h-100 d-flex align-items-center flex-grow-1 py-5 justify-content-center"
          @click.prevent="$emit('select', 'client')">
