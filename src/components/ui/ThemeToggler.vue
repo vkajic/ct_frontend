0b9a89fcd32eaee889c937ddcf01cee6735212ad
igor-lid <onaj.igor@gmail.com>
2020-04-29 18:01:13 +0200

    wip - add theme toggler

---
+++ src/components/ui/ThemeToggler.vue
@@ -0,0 +1,33 @@
+<template>
+  <sun-icon class="theme-icon" v-if="!isDark" @click="toggleDarkMode"></sun-icon>
+  <moon-icon class="theme-icon" v-else @click="toggleDarkMode"></moon-icon>
+</template>
+<script>
+import { SunIcon, MoonIcon } from 'vue-feather-icons';
+
+export default {
+  name: 'ThemeToggler',
+  components: {
+    SunIcon,
+    MoonIcon,
+  },
+  data() {
+    return {
+      isDark: false,
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      const { body } = document;
+      this.isDark = !this.isDark;
+      body.classList.toggle('dark-theme');
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+  .theme-icon {
+    margin-left: 1rem;
+    cursor: pointer;
+  }
+</style>
