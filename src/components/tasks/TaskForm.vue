e029ae082b2b33ad573bd78ba6b345a250b88794
Vedran <info@cryptotask.org>
2020-05-23 20:23:30 +0200

    added onetime task type

--- src/components/tasks/TaskForm.vue
+++ src/components/tasks/TaskForm.vue
@@ -155,6 +155,10 @@ export default {
           value: 'parttime',
           text: 'Part Time',
         },
+        {
+          value: 'onetime',
+          text: 'One Time',
+        },
       ],
       locations: [
         {
