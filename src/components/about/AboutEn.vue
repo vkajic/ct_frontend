0ba34d25c6183c33b4ce7ca008513e58bd902af1
mario <tadic.mario@gmail.com>
2021-01-20 19:22:52 +0100

    about page fix

--- src/components/about/AboutEn.vue
+++ src/components/about/AboutEn.vue
@@ -2,30 +2,39 @@
   <div>
     <h1 class="mb-4 font-weight-bold">How It Works</h1>
     <div class="mb-4 lead">
-      <p>We are a decentralized freelance market ecosystem lowering fees
-        by directly connecting hirees and
-        applicants peer to peer. Job disputes are resolved much faster and cheaper,
-        while your reputation is stored on
-        blockchain and there is no arbitrary censorship or hidden tampering,
-        such as hidden boosting, something that
-        centralized platforms are infamous for.<br/>
-        Find work online &#8211; work from home on blockchain, app dev,
-        graphic design, general development and
-        many other freelance job categories.<br/>
-        Looking for a site to post a job? You are at the right place! If you are
-        looking for remote freelancers to hire
-        &#8211; post a job online and find a remote designer, programmer, coder,
-        writer or any other expert.</p>
+      <p>We are a decentralized blockchain freelance market ecosystem lowering fees by directly connecting hirees and
+        applicants peer to peer. Job disputes are resolved much faster and cheaper, while your reputation is stored on
+        <a
+          href="https://finance.yahoo.com/news/ternity-launches-inaugural-universe-one-070000114.html"><em>blockchain</em></a>
+        and there is no arbitrary censorship or hidden tampering, such as hidden boosting, something that centralized
+        platforms are infamous for.<br>
+        Find work online – work from home on crypto, blockchain, app dev, graphic design, general development and many
+        other freelance job categories.<br>
+        Looking for a site to post a job? You are at the right place! If you are looking for remote freelancers to hire
+        – post a job online and find a remote designer, programmer, coder, writer or any other expert.</p>
+      <p>Browse crypto gigs, use escrow and pay in bitcoin, ethereum, aeternity, stable coin or other crypto – loans
+        available from our partner <em><a
+          href="https://medium.com/smartcredit-io/cryptotask-partners-with-smartcredit-io-fa048b9cecee">SmartCredit</a></em>.
+        Welcome to the future of crypto freelancing.</p>
       <p><a href="https://medium.com/@info_97177/the-largest-ever-work-from-home-experiment-part-2-55ca0eec4700">Corona
-        virus and freelancing:</a><br/>
+        virus and freelancing:</a><br>
         <a href="https://medium.com/@info_97177/the-largest-ever-work-from-home-experiment-part-2-55ca0eec4700"
-           target="_blank" rel="noopener noreferrer">
-          <img src="https://miro.medium.com/max/1280/0*SCfMcXdlTP6JLZyW"
-               alt="Corona virus and freelancing" width="500"/></a></p>
-      <p>Read more about us <strong><em>
-        <a href="http://about.cryptotask.org" target="_blank"
-           rel="noopener noreferrer">here</a></em></strong></p>
-      <p>Contact: <em>pozdrav -at- freelance.hr</em></p>
+           target="_blank" rel="noopener noreferrer"><img src="https://miro.medium.com/max/1280/0*SCfMcXdlTP6JLZyW"
+                                                          alt="Corona virus and freelancing" width="500"></a></p>
+      <p>Our explainer vid:</p>
+      <p>
+        <iframe title="CryptoTask Official Video" width="500" height="281"
+                src="https://www.youtube.com/embed/WHLkADOL3Nk?feature=oembed" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen=""></iframe>
+      </p>
+      <p>Read more about us <strong><em><a href="http://about.cryptotask.org" target="_blank" rel="noopener noreferrer">here</a></em></strong>
+      </p>
+      <p>Read our Medium <strong><em><a href="https://medium.com/@cryptotask" target="_blank" rel="noopener noreferrer">here</a></em></strong>
+      </p>
+      <p>Join our telegram <a href="https://t.me/cryptotaskofficial">https://t.me/cryptotaskofficial</a></p>
+      <p>Contact: <em>help -at- cryptotask.org<br>
+      </em></p>
     </div>
   </div>
 </template>
